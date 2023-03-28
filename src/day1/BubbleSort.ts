fe98c46f62dca380cbbc7649ecdae16798185268
Arley Rico <arleyrc09@gmail.com>
2023-03-27 22:29:03 -0500

    Add Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+  let aux;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - (i + 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        aux = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = aux;
+      }
+    }
+  }
 }
