daae33f6699dde5663b9532ae29c426fc32f7f5f
Arley Rico <arleyrc09@gmail.com>
2023-04-13 18:54:13 -0500

    Finish recursion implementation

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,11 +1,30 @@
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+function convertSeen(seen: boolean[][]): void {
+  let output = "";
+  for (let i = 0; i < seen.length; i++) {
+    output += seen[i].join(",") + "\n";
+  }
+
+  console.log(output);
+}
+
 function walk(
   maze: string[],
   wall: string,
   curr: Point,
   end: Point,
-  seen: boolean[][],
+  seen: any[][],
+  path: Point[],
+  count: number,
 ): boolean {
-  // first case - off the map
+  // 1. BASE CASES
+  //  - off the map
   if (
     curr.x < 0 ||
     curr.x > maze[0].length ||
@@ -16,14 +35,41 @@ function walk(
   }
 
   // on a wall
-  if (maze[curr.y][curr.x] === wall) return false;
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
 
   // on the end
-  if (curr.x === end.x && curr.y === end.y) return true;
+  if (curr.x === end.x && curr.y === end.y) {
+    path.push(end);
+    convertSeen(seen);
+    return true;
+  }
 
   // is seen
-  if (seen[curr.y][curr.x]) return false;
+  if (seen[curr.y][curr.x]) {
+    return false;
+  }
 
+  // 3. RECURSE
+  // pre
+  count++;
+  seen[curr.y][curr.x] = count;
+  path.push(curr);
+
+  //recurse
+  for (let i = 0; i < dir.length; i++) {
+    const [x, y] = dir[i];
+    if (
+      walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path, count)
+    ) {
+      convertSeen(seen);
+      return true;
+    }
+  }
+
+  // post
+  path.pop();
   return false;
 }
 
@@ -32,4 +78,15 @@ export default function solve(
   wall: string,
   start: Point,
   end: Point,
-): Point[] {}
+): Point[] {
+  const seen: any[][] = [];
+  const path: Point[] = [];
+
+  for (let i = 0; i < maze.length; i++) {
+    seen.push(new Array(maze[0].length).fill(false));
+  }
+
+  walk(maze, wall, start, end, seen, path, 0);
+
+  return path;
+}
